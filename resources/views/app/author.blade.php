c1be2f92f98312ced3a2004b84e22a6dad7e64e5
Stan Fortonski <contact@stanfortonski.dev>
2021-01-29 21:58:41 +0100

    Better look authors page

--- resources/views/app/author.blade.php
+++ resources/views/app/author.blade.php
@@ -6,13 +6,13 @@
 @section('content')
 <div class="row">
     <div class="col-12">
-        <figure class="figure-author">
-            @if(!empty($user->thumbnail_path))
-                <img src="{{ $user->avatar }}" alt="{{ $user->full_name }} - {{ config('app.name') }}" itemprop="image" class="img-fluid" width="144" height="144">
-            @endif
-            <figcaption><h1>{{ $user->full_name }}</h1></figcaption>
-        </figure>
-        <p>{{ $content->content ?? '' }}</p>
+        @if(!empty($user->thumbnail_path))
+            <img src="{{ $user->avatar }}" alt="{{ $user->full_name }} - {{ config('app.name') }}" itemprop="image" class="img-fluid m-2" width="144" height="144" style="vertical-align: top;">
+        @endif
+        <div class="d-inline-block">
+            <h1 style="display: inline;">{{ $user->full_name }}</h1><br>
+            {{ $content->content ?? '' }}
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/components/author-note.blade.php
+++ resources/views/components/author-note.blade.php
@@ -1,13 +1,15 @@
 <div class="card" itemprop="author" itemscope itemtype="http://schema.org/Person">
-    <div class="card-body p-2">
+    <div class="card-body py-2 px-0">
         <div class="col">
-            <figure class="author-figure">
+            <figure class="figure-author">
                 @if(!empty($author->thumbnail_path))
                     <img src="{{ $author->avatar }}" alt="{{ $author->full_name }} - {{ config('app.name') }}" itemprop="image" class="img-fluid" width="144" height="144">
                 @endif
-                <figcaption>{{ $author->full_name }}</figcaption>
+                <figcaption class="ml-3">
+                    <h5 itemprop="name">{{ $author->full_name }}</h5>
+                    <span itemprop="description">{{ $content->description ?? 'dassssssssssssssssssssssssssssss' }}</span>
+                </figcaption>
             </figure>
-            <span itemprop="description">{{ $content->description ?? '' }}</span>
             <p><a href="{{ route('author', [app()->getLocale(), $author->url]) }}">{{ __('Read More about author') }}</a></p>
         </div>
     </div>
