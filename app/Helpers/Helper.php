9a9810895493bfe92f432118d60bd34ae43667a4
Stan Fortonski <contact@stanfortonski.dev>
2021-02-07 12:24:07 +0100

    StripTags helper update

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -6,7 +6,7 @@ class Helper
 {
     static public function stripTags(string $str): string
     {
-        return strip_tags($str, '<h1><h2><h3><h4><h5><h6><p><br><b><i><strong><mark><a><span><div><li><ul><ol><table><thead><td><tr><th><tbody><blockquote><section>');
+        return strip_tags($str, '<h1><h2><h3><h4><h5><h6><p><br><b><i><strong><mark><a><span><div><li><ul><ol><table><thead><td><tr><th><tbody><blockquote><section><code><pre>');
     }
 
     static public function properUrl(string $str){
