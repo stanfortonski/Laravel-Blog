3ff079feb8357b09aab682e7f0ca3af3c6033970
Stan Fortonski <contact@stanfortonski.dev>
2021-02-27 14:47:46 +0100

    Better NoImage

--- resources/views/components/no-image.blade.php
+++ resources/views/components/no-image.blade.php
@@ -1,4 +1,4 @@
-<svg class="no-image bd-placeholder-img align-self-center mr-3" width="{{ $width }}" height="{{ $height }}" style="min-width: {{ $width }}px;" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: {{ $content->title }}" preserveAspectRatio="xMidYMid slice" role="img">
+<svg class="no-image bd-placeholder-img align-self-center mr-3" @if($width) width="{{ $width }}" style="min-width: {{ $width }}px;" @endif @if($height) height="{{ $height }}" @endif xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: {{ $content->title }}" preserveAspectRatio="xMidYMid slice" role="img">
     <title>{{ $content->title }}</title>
     <rect width="100%" height="100%" fill="{{ $background ?? config('blog.no-image.background') }}"/>
     <text x="50%" y="50%" dy=".3em" fill="{{ $color ?? config('blog.no-image.color') }}">{{ strtoupper($content->title[0]) }}</text>
