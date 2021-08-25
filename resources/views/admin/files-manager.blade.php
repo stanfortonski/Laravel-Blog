b60a402a34fe1ee15fc767e70d43e5f498253c10
Stan Fortoński <contact@stanfortonski.dev>
2021-08-25 16:51:23 +0200

    Hotfix

--- resources/views/admin/files-manager.blade.php
+++ resources/views/admin/files-manager.blade.php
@@ -6,7 +6,7 @@
 <div class="row justify-content-center">
     <div class="col-12">
         <iframe src="{{ route('admin.unisharp.lfm.show') }}" style="width: 100%; height: 600px; overflow: hidden; border: none;"></iframe>
-        <a href="{{ route('admin.unisharp.lfm.show') }}" class="btn btn-primary mt-4" target="_blank">{{ __('Open in new card') }}</a>
+        <a href="{{ route('admin.unisharp.lfm.show') }}" class="btn btn-primary my-4" target="_blank">{{ __('Open in new card') }}</a>
     </div>
 </div>
 @endsection
